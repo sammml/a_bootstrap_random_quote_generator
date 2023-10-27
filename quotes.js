099f6d09d8f8e3c4afd8e8e36801ba60685d22bd
cheesepuff <cheesepuff@cheesepuffs-mbp.mynetworksettings.com>
2023-10-27 15:40:02 -0400

    Add random quote generator

---
+++ quotes.js
@@ -0,0 +1,61 @@
+const QUOTEBANK = [{
+quote: "The only way to do great work is to love what you do.",
+author: "Steve Jobs"},
+{
+quote: "In three words I can sum up everything I've learned about life: it goes on.",
+author: "Robert Frost"},
+{
+quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+author: "Winston Churchill"},
+{
+quote: "The only limit to our realization of tomorrow will be our doubts of today.",
+author: "Franklin D. Roosevelt"},
+{
+quote: "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+author: "Martin Luther King Jr."},
+{
+quote: "In the middle of every difficulty lies opportunity.",
+author: "Albert Einstein"},
+{
+quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+author: "Nelson Mandela"},
+{
+quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+author: "Ralph Waldo Emerson"},
+{
+quote: "The only true wisdom is in knowing you know nothing.",
+author: "Socrates"},
+{
+quote: "Change your thoughts and you change your world.",
+author: "Norman Vincent Peale"},
+{
+quote: "The only thing necessary for the triumph of evil is for good men to do nothing.",
+author: "Edmund Burke"},
+{
+quote: "Life is what happens when you're busy making other plans.",
+author: "John Lennon"},
+{
+quote: "To succeed in life, you need two things: ignorance and confidence.",
+author: "Mark Twain"},
+{
+quote: "The greatest pleasure in life is doing what people say you cannot do.",
+author: "Walter Bagehot"},
+{
+quote: "If you want to go fast, go alone. If you want to go far, go together.",
+author: "African Proverb"},
+{
+quote: "Happiness is not something ready made. It comes from your own actions.",
+author: "Dalai Lama"},
+{
+quote: "The only person you are destined to become is the person you decide to be.",
+author: "Ralph Waldo Emerson"},
+{
+quote: "The best time to plant a tree was 20 years ago. The second best time is now.",
+author: "Chinese Proverb"},
+{
+quote: "The only limit to our realization of tomorrow will be our doubts of today.",
+author: "Franklin D. Roosevelt"},
+{
+quote: "Don't watch the clock; do what it does. Keep going.",
+author: "Sam Levenson"}
+]
